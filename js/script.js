531d8f706ac2f404e0da8192bd7b87c098fc8325
JustaveD <yeumytoan0101@gmail.com>
2021-09-18 19:32:21 +0700

    search form and search responsive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
+// Toggle bars in header responsive
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = ()=>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
+}
+window.onscroll = ()=>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+// Search form apperence animation
+let searchIcon = document.querySelector('#search-icon');
+let searchForm = document.querySelector('#search-form');
+let searchClose = document.querySelector('#close');
+searchIcon.onclick = () =>{
+  searchForm.classList.add('active');
+}
+searchClose.onclick = () =>{
+  searchForm.classList.remove('active');
 }
